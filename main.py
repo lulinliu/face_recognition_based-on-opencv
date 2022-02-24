45b4222c3a11f9a38f3f91e5070d1fadb5c666a4
lulinliu <93459201+lulinliu@users.noreply.github.com>
2022-02-24 22:08:49 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+import os
+
+
+def face_detect(img):
+    gary = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    #opencv处理
+    face_detect = cv.CascadeClassifier('/Users/liululin/PycharmProjects/pythonProject16/venv/lib/python3.9/site-packages/cv2/data/haarcascade_frontalface_alt2.xml')
+    face = face_detect.detectMultiScale(gary,1.01,150,0,(10,10),(400,400))
+    #根据xywh，进行opencv算法
+    for x,y,w,h in face:
+        if w>=128 and h>=128:
+            cv.rectangle(img,(x,y),(x+w,y+h),color=(0,255,0),thickness=2)
+
+    i = 1
+    for face__ in face:
+        img_ = img[face__[1]:face__[1] + face__[3], face__[0]:face__[0] + face__[2]]
+        # 保存图像
+        i = i + 1
+        cv.imwrite(target_path2, img_)
+
+
+source_path = '/Users/liululin/Downloads/archive (3)/images/'
+target_dir_name = 'target'
+target_path = source_path + target_dir_name
+if not os.path.exists(target_path):
+    os.makedirs(target_path)
+
+for i in range(852):
+    target_path2 = target_path +'/'+ 'NEW' + str(i) +'.png'
+    file_path = source_path + 'maksssksksss' + str(i) + '.png'
+    img = cv.imread(file_path)
+    face_detect(img)
+
